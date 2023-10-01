ec1b1dad8eb6598f1affe6789e2a55943eb5e5b0
Shakil53 <shakilahmed.dcc@gmail.com>
2023-10-01 13:27:28 +0600

    course id created

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u7o1gfd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,6 +39,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        app.get('/courses/:id'), async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await courseCollection.findOne(query)
+            res.send(result)
+        }
 
 
 
